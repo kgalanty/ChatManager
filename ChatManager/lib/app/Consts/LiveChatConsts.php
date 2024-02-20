5f7de60a61ed4bb17ffe4d6797485efbf23e4bb2
kgalanty <dataz.karas@gmail.com>
2024-02-20 19:22:19 +0100

    Update api keys

--- ChatManager/lib/app/Consts/LiveChatConsts.php
+++ ChatManager/lib/app/Consts/LiveChatConsts.php
@@ -2,6 +2,6 @@
 namespace WHMCS\Module\Addon\ChatManager\app\Consts;
 class LiveChatConsts
 {
-    const LIVECHAT_LOGIN = '0fce32f4-5759-4609-b155-4ec9435ed7b1';
-    const LIVECHAT_PASS = 'dal:-9MPtP5TFXnIbD8C1Dq3rqW8XX4';
+    const LIVECHAT_LOGIN = 'ddee54ad-4ca5-4e61-8193-e8b16fe51496';
+    const LIVECHAT_PASS = 'dal:8-UKC9hInMaTwnmgVEjkRhokjBI';
 }
